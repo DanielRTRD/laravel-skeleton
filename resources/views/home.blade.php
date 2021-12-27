22a7d25f7b53f9a76ddcc65268111416a747c6ea
Daniel S. Billing <daniel@infihex.com>
2021-12-27 16:52:32 +0100

    Added home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en" class="scroll-smooth">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="Daniel S. Billing" />
+        <title>Infihex</title>
+        <!-- Favicon-->
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+        <link rel="manifest" href="site.webmanifest">
+        <!-- CSS -->
+        <link href="./css/app.css" rel="stylesheet" />
+        <!-- JS -->
+        <script defer src="./js/app.js"></script>
+    </head>
+    <body id="page-top" class="h-full font-sans antialiased text-gray-200 bg-gray-900">
+        
+        <header class="text-gray-200 body-font">
+            <div class="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
+              <a class="flex items-center mb-4 font-bold md:mb-0">
+                <span class="text-2xl text-orange-400">Infihex</span>
+              </a>
+              <nav class="flex flex-wrap items-center justify-center text-base text-gray-500 md:ml-auto">
+                <a class="mr-5 hover:text-orange-400" href="https://github.com/Infihex">Github</a>
+                <a class="hover:text-orange-400" href="https://status.infihex.no/">Operational Status</a>
+              </nav>
+              <!--<button class="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-600 border-0 rounded focus:outline-none hover:bg-gray-500 md:mt-0">Button
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </button>-->
+            </div>
+        </header>
+
+        <section class="flex items-center justify-center w-full h-auto sm:h-screen">
+            <div class="max-w-6xl mx-auto">
+
+                <div class="container max-w-lg px-4 py-32 mx-auto text-left md:max-w-none md:text-center">
+                    <h1 class="text-5xl font-extrabold tracking-tight text-left text-gray-100 leading-12 md:text-center sm:leading-none md:text-6xl lg:text-7xl xl:text-8xl">
+                        We do <span class="text-transparent underline bg-clip-text bg-gradient-to-l from-indigo-500 to-indigo-400 decoration-wavy decoration-2 decoration-indigo-400">magic</span> with <span class="text-transparent underline bg-clip-text bg-gradient-to-l from-orange-500 to-orange-400 decoration-2 decoration-solid decoration-orange-400">code</span> to <br>make your <span class="text-transparent underline bg-clip-text bg-gradient-to-l from-blue-600 to-blue-400 decoration-2 decoration-dashed decoration-indigo-400">dreams</span> come true!
+                    </h1>
+                    <div class="max-w-xl mx-auto mt-5 text-gray-500 md:mt-12 md:text-center lg:text-lg">
+                        We're a small team but passionate team based in Norway. We're here to help you build your next product.
+                        <!--We do most of our work in code form, but we also do hosting, management, administration, support and other services as well.-->
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+        <section class="bg-black/50 pt-7 pb-14">
+            <div class="container px-8 mx-auto sm:px-12 lg:px-20">
+                <h1 class="font-bold tracking-wide text-center uppercase text-md mb-7">Trusted by these clients</h1>
+                <div class="grid items-center justify-center grid-cols-12 gap-y-8 grayscale">
+                    <div class="flex items-center justify-center col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-2">
+                        <img src="./img/brottumlan.png" alt="" class="block object-contain h-12">
+                    </div>
+                    <div class="flex items-center justify-center col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-2">
+                        <img src="./img/chaniasegwaytours.png" alt="" class="block object-contain h-12">
+                    </div>
+                    <div class="flex items-center justify-center col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-2">
+                        <img src="./img/downlinkdg.png" alt="" class="block object-contain h-12">
+                    </div>
+                    <div class="flex items-center justify-center col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-2">
+                        <img src="./img/dreamzesports.png" alt="" class="block object-contain h-12">
+                    </div>
+                    <div class="flex items-center justify-center col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-2">
+                        <img src="./img/viqueens.png" alt="" class="block object-contain h-12">
+                    </div>
+                    <div class="flex items-center justify-center col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-2">
+                        <img src="./img/yogstation.png" alt="" class="block object-contain h-12">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="w-full px-6 py-12">
+            <div class="max-w-6xl mx-auto">
+                <div class="container flex flex-col items-start mx-auto lg:items-center">
+                    <p class="relative flex items-start justify-start w-full text-lg font-bold tracking-wider text-orange-500 uppercase lg:justify-center lg:items-center">
+                        Don't just take our word for it
+                    </p>
+
+                    <h2 class="relative flex items-start justify-start w-full max-w-3xl text-5xl font-bold lg:justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="absolute right-0 hidden w-12 h-12 -mt-2 -mr-16 lg:inline-block" viewBox="0 0 975.036 975.036">
+                            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z">
+                            </path>
+                        </svg>
+                        See what others are saying
+                    </h2>
+                    
+                    <div class="block w-full h-0.5 max-w-lg mt-6 bg-gray-700 rounded-full"></div>
+
+                    <div class="items-center justify-center w-full mt-12 mb-4 lg:flex">
+                        <div class="flex flex-col items-start justify-start w-full h-auto mb-12 lg:w-1/3 lg:mb-0">
+                            <div class="flex items-center justify-center">
+                                <div class="w-16 h-16 mr-4 overflow-hidden bg-gray-700 rounded-full grayscale">
+                                    <img src="./img/runarklemetsdal.png" class="object-cover w-full h-full">
+                                </div>
+                                <div class="flex flex-col items-start justify-center">
+                                    <h4 class="font-bold text-gray-300">Runar Klemetsdal</h4>
+                                    <p class="text-sm text-gray-500">Administration</p>
+                                </div>
+                            </div>
+                            <blockquote class="mt-8 text-lg text-gray-400"><span class="mr-2 text-2xl text-orange-400">&QUOT;</span>Without Infihex developing LANMS, it would not have been as easy to keep track of visitors, tickets and seats for our events!<span class="ml-2 text-2xl text-orange-400">&QUOT;</span></blockquote>
+                        </div>
+                        <div class="flex flex-col items-start justify-start w-full h-auto px-0 mx-0 mb-12 border-l border-r border-transparent lg:w-1/3 lg:mb-0 lg:px-8 lg:mx-8 lg:border-gray-700">
+                            <div class="flex items-center justify-center">
+                                <div class="w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full grayscale">
+                                    <img src="./img/andreasovern.jpeg" class="object-cover w-full h-full">
+                                </div>
+                                <div class="flex flex-col items-start justify-center">
+                                    <h4 class="font-bold text-gray-300">Andreas Overn</h4>
+                                    <p class="text-sm text-gray-500">Administration</p>
+                                </div>
+                            </div>
+                            <blockquote class="mt-8 text-lg text-gray-400"><span class="mr-2 text-2xl text-orange-400">&QUOT;</span>Infihex has through 2020 developed a website for DreamZ Esports. The project has been to expand and continue our new website for DreamZ Esports. We are looking forward to the future!<span class="ml-2 text-2xl text-orange-400">&QUOT;</span></blockquote>
+                        </div>
+                        <div class="flex flex-col items-start justify-start w-full h-auto lg:w-1/3">
+                            <div class="flex items-center justify-center">
+                                <div class="w-16 h-16 mr-4 overflow-hidden bg-gray-200 rounded-full grayscale">
+                                    <img src="./img/rosshardy.png" class="object-cover w-full h-full">
+                                </div>
+                                <div class="flex flex-col items-start justify-center">
+                                    <h4 class="font-bold text-gray-300">Ross Hardy</h4>
+                                    <p class="text-sm text-gray-500">Web administration</p>
+                                </div>
+                            </div>
+                            <blockquote class="mt-8 text-lg text-gray-400"><span class="mr-2 text-2xl text-orange-400">&QUOT;</span>Daniel, you're one of the friendliest developers I've ever met. Usually they're all stubborn and boring. Thank you for your great development and support!<span class="ml-2 text-2xl text-orange-400">&QUOT;</span></blockquote>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="w-full px-8 py-16 bg-black/20 xl:px-8">
+            <div class="max-w-5xl mx-auto">
+                <div class="flex flex-col items-center md:flex-row">
+
+                    <div class="w-full space-y-5 md:w-3/5 md:pr-10 lg:pr-16">
+                        <p class="font-medium text-orange-400 uppercase">Get in touch</p>
+                        <h2 class="text-2xl font-extrabold leading-none sm:text-3xl md:text-5xl">
+                            Want to do business with us?
+                        </h2>
+                        <p class="text-xl text-gray-400 md:pr-16">
+                            We're excited to hear from you about your next project or anything else! If you have any questions, please don't hesitate to contact us.
+                        </p>
+                    </div>
+
+                    <!--<div class="w-full mt-16 md:mt-0 md:w-2/5">
+                        <div class="relative z-10 h-auto p-8 py-10 overflow-hidden bg-black border-b-2 border-orange-500 rounded-lg shadow-2xl shadow-black px-7">
+                            <h3 class="mb-6 text-2xl font-medium text-center">Leave your details here</h3>
+                            <input type="text" name="name" class="block w-full px-4 py-3 mb-4 text-gray-900 border-2 border-transparent border-gray-200 rounded-lg focus:ring focus:ring-orange-500 focus:outline-none" placeholder="Full Name">
+                            <input type="email" name="email" class="block w-full px-4 py-3 mb-4 text-gray-900 border-2 border-transparent border-gray-200 rounded-lg focus:ring focus:ring-orange-500 focus:outline-none" placeholder="Email address">
+                            <input type="tel" name="phone" class="block w-full px-4 py-3 mb-4 text-gray-900 border-2 border-transparent border-gray-200 rounded-lg focus:ring focus:ring-orange-500 focus:outline-none" placeholder="Phone number">
+                            <div class="block">
+                                <button class="w-full px-2 py-3 font-medium text-white uppercase bg-orange-500 rounded-lg"><svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-5 h-5 mb-1 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+                                  </svg>Contact me</button>
+                            </div>
+                        </div>-->
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <footer class="bg-black">
+            <div class="container flex flex-col items-center px-5 py-8 mx-auto sm:flex-row">
+              <a class="flex items-center justify-center font-bold text-gray-400 md:justify-start">
+                <span class="text-xl text-orange-400">Infihex</span>
+              </a>
+              <p class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0">&copy; 2022 Infihex. All rights reserved.</p>
+              </p>
+              <span class="inline-flex justify-center mt-4 sm:ml-auto sm:mt-0 sm:justify-start">
+                <a class="text-gray-500 hover:text-orange-400" href="https://www.facebook.com/infihex">
+                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-3 text-gray-500 hover:text-orange-400" href="https://www.twitter.com/infihex">
+                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-3 text-gray-500 hover:text-orange-400" href="https://www.instagram.com/infihex">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                  </svg>
+                </a>
+                <a class="ml-3 text-gray-500 hover:text-orange-400" href="https://www.youtube.com/c/infihexsupport">
+                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="-35.20005 -41.33325 305.0671 247.9995">
+                        <path d="M229.763 25.817c-2.699-10.162-10.65-18.165-20.747-20.881C190.716 0 117.333 0 117.333 0S43.951 0 25.651 4.936C15.554 7.652 7.602 15.655 4.904 25.817 0 44.237 0 82.667 0 82.667s0 38.43 4.904 56.85c2.698 10.162 10.65 18.164 20.747 20.881 18.3 4.935 91.682 4.935 91.682 4.935s73.383 0 91.683-4.935c10.097-2.717 18.048-10.72 20.747-20.88 4.904-18.422 4.904-56.851 4.904-56.851s0-38.43-4.904-56.85"/>
+                        <path d="M93.333 117.558l61.334-34.89-61.334-34.893z" fill="#000"/>
+                    </svg>
+                </a>
+              </span>
+            </div>
+        </footer>
+    </body>
+</html>
